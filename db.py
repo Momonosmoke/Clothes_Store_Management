604ec11a7630b45184f4037582a9bf403a3eb4ac
Nguyễn Tuấn Khải <nkhai462@gmail.com>
2022-05-01 08:34:25 +0700

    Update db.py

--- db.py
+++ db.py
@@ -6,27 +6,28 @@ class Database:
         self.conn = sqlite3.connect(db)
         self.cur = self.conn.cursor()
         self.cur.execute(
-            "CREATE TABLE IF NOT EXISTS clothes (id INTEGER PRIMARY KEY, clothes text, customer text, order_time datetime, retailer text, price text)")
+            "CREATE TABLE IF NOT EXISTS parts (id INTEGER PRIMARY KEY, part text, customer text, retailer text, price text, order_time text)")
         self.conn.commit()
 
     def fetch(self):
-        self.cur.execute("SELECT * FROM clothes")
+        self.cur.execute("SELECT * FROM parts")
         rows = self.cur.fetchall()
         return rows
 
-    def insert(self, clothes, customer, order_time, retailer, price):
-        self.cur.execute("INSERT INTO clothes VALUES (NULL, ?, ?, ?, ?, ?)",
-                         (clothes, customer, order_time, retailer, price))
+    def insert(self, part, customer, retailer, price, order_time):
+        self.cur.execute("INSERT INTO parts VALUES (NULL, ?, ?, ?, ?, ?)",
+                         (part, customer, retailer, price, order_time))
         self.conn.commit()
 
     def remove(self, id):
-        self.cur.execute("DELETE FROM clothess WHERE id=?", (id,))
+        self.cur.execute("DELETE FROM parts WHERE id=?", (id,))
         self.conn.commit()
 
-    def update(self, id, clothes, customer, order_time, retailer, price):
-        self.cur.execute("UPDATE clothess SET clothes = ?, customer = ?, retailer = ?, price = ?, order_time = ? WHERE id = ?",
-                         (clothes, customer, order_time, retailer, price, order_time, id))
+    def update(self, id, part, customer, retailer, price, order_time):
+        self.cur.execute("UPDATE parts SET part = ?, customer = ?, retailer = ?, price = ?, order_time = ? WHERE id = ?",
+                         (part, customer, retailer, price, order_time, id))
         self.conn.commit()
 
     def __del__(self):
         self.conn.close()
+
