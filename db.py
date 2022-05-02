fda5095c95acabc6e5cd919a81c65dc4feac8d80
Nguyễn Tuấn Khải <nkhai462@gmail.com>
2022-05-02 19:02:13 +0700

    Update db.py

--- db.py
+++ db.py
@@ -1,33 +1,30 @@
 import sqlite3
-
-
 class Database:
     def __init__(self, db):
         self.conn = sqlite3.connect(db)
         self.cur = self.conn.cursor()
         self.cur.execute(
-            "CREATE TABLE IF NOT EXISTS parts (id INTEGER PRIMARY KEY, part text, customer text, retailer text, price text, order_time text)")
+            "CREATE TABLE IF NOT EXISTS clothes (id INTEGER PRIMARY KEY, clothes text, customer text, retailer text, price text, order_time datetime)")
         self.conn.commit()
 
     def fetch(self):
-        self.cur.execute("SELECT * FROM parts")
+        self.cur.execute("SELECT * FROM clothes")
         rows = self.cur.fetchall()
         return rows
 
-    def insert(self, part, customer, retailer, price, order_time):
-        self.cur.execute("INSERT INTO parts VALUES (NULL, ?, ?, ?, ?, ?)",
-                         (part, customer, retailer, price, order_time))
+    def insert(self, clothes, customer, retailer, price, order_time):
+        self.cur.execute("INSERT INTO clothes VALUES (NULL, ?, ?, ?, ?, ?)",
+                         (clothes, customer, retailer, price, order_time))
         self.conn.commit()
 
     def remove(self, id):
-        self.cur.execute("DELETE FROM parts WHERE id=?", (id,))
+        self.cur.execute("DELETE FROM clothes WHERE id=?", (id,))
         self.conn.commit()
 
-    def update(self, id, part, customer, retailer, price, order_time):
-        self.cur.execute("UPDATE parts SET part = ?, customer = ?, retailer = ?, price = ?, order_time = ? WHERE id = ?",
-                         (part, customer, retailer, price, order_time, id))
+    def update(self, id, clothes, customer, retailer, price, order_time):
+        self.cur.execute("UPDATE clothes SET clothes = ?, customer = ?, price = ?, order_time = ?, retailer = ? WHERE id = ?",
+                         (clothes, customer, price, order_time, retailer, id))
         self.conn.commit()
 
     def __del__(self):
         self.conn.close()
-
